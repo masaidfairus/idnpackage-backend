3eed6b1a12ea92b0f5eafda6cb3a428c626350a0
IbnuMuhadzdzib <ibnulaginugas@gmail.com>
2026-07-20 13:17:48 +0700

    fix: change pickedUpDate validation to IsDateString to accept ISO string from frontend

--- src/packages/dto/update-package.dto.ts
+++ src/packages/dto/update-package.dto.ts
@@ -1,11 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePackageDto } from './create-package.dto';
-import { IsDate, IsOptional } from 'class-validator';
-import { Type } from 'class-transformer';
+import { IsDateString, IsOptional } from 'class-validator';
 
 export class UpdatePackageDto extends PartialType(CreatePackageDto) {
   @IsOptional()
-  @Type(() => Date)
-  @IsDate()
-  pickedUpDate?: Date | null;
+  @IsDateString()
+  pickedUpDate?: string | null;
 }
