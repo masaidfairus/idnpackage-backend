271d5c984b70c2ddae971244f6eb0a44effaf7ed
IbnuMuhadzdzib <ibnulaginugas@gmail.com>
2026-09-03 22:46:12 +0700

    update: cors origin true

--- src/main.ts
+++ src/main.ts
@@ -17,10 +17,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: [
-    'http://localhost:5173',
-    /https:\/\/.*\.vercel\.app$/,   // wildcard semua domain vercel
-  ],
+  origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
